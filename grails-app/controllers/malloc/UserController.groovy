c18d1ce90e3803386a1959943a7828754f5e4511
mawek <mawek@dracik>
2011-04-07 19:01:12 +0200

    init commit

---
+++ grails-app/controllers/malloc/UserController.groovy
@@ -0,0 +1,6 @@
+package malloc
+
+class UserController {
+
+    def scaffold = User
+}
---
+++ grails-app/domain/malloc/User.groovy
@@ -0,0 +1,12 @@
+package malloc
+
+class User {
+
+    static constraints = {
+    }
+	
+	String name
+	String surname
+	String nickname	
+	String email
+}
